1eb68ce3553023d2d015f945009e2b65e1259ed8
George Brata <bratageorge@gmail.com>
2022-06-04 13:29:16 +0300

    small update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "georgebrata.ro",
+    title: "personal-website",
     htmlAttrs: {
       lang: "en",
     },
