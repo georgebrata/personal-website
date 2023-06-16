d394957624a86990096bade3cca3f4634fc74c71
George Brata <bratageorge@gmail.com>
2023-06-16 13:05:35 +0300

    small updates

--- data/sitemetainfo.js
+++ data/sitemetainfo.js
@@ -1,21 +1,20 @@
 const siteMetaInfo = {
-  title: "George Brata 🕸 Developer",
+  title: "George Brata | Web Developer",
   author: "George Brata",
   author_image: "/author/",
-  position: "Frontend 🕸 Developer",
+  position: "Frontend Web Developer",
   location: "Romania",
   headerTitle: "georgebrata.ro",
-  slogan: "Talk is cheap; show me the code review",
   description:
     "Do you need a cool, modern website that actually sells? Contact me!",
   language: "en-us",
-  theme: "system", // system, dark or light
+  theme: "dark", // system / dark / light
   siteUrl: "https://georgebrata.ro",
   siteRepo: "#",
   siteLogo: "../assets/author-1.png",
   image: "~/assets/author-1.png",
   socialBanner: "/static/images/twitter-card.png",
-  email: "gmail@georgebrata.ro"
+  email: "george@brata.ro"
 };
 
 export default siteMetaInfo;
