fc64cd3140baeb61b315312cbd95436f6faef868
George Brata <bratageorge@gmail.com>
2024-06-30 17:31:41 +0300

    Projects update

--- pages/meet.vue
+++ pages/meet.vue
@@ -17,7 +17,6 @@
             <!-- Calendly inline widget begin -->
             <div class="calendly-inline-widget" data-url="https://calendly.com/bratageorge/15min?hide_gdpr_banner=1" style="min-width:320px;height:700px;"></div>
             <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-            <!-- Calendly inline widget end -->
           </div>
         </div>
       </main>
